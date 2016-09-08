3591cebb81c86d81d498e6028f453df73a75bee1
Patrick Fitzgerald <fitzgeraldpk@gmail.com>
2016-09-08 08:10:28 +0100

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -4,4 +4,5 @@ tar xzf redis-3.2.3.tar.gz
 cd redis-3.2.3
 make
 cd ~/redis-3.2.3/src
+#run in unprotected mode and accept connections - development only
 ./redis-server --protected-mode no --bind 0.0.0.0 &
